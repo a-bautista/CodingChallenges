20b5b8cd723b15d4e03e2d8fc2fd1f608897cd64
Alejandro Bautista Ramos <alexbr9007@hotmail.com>
2020-06-10 20:31:34 +0000

    MaximumSwap.py edited online with Bitbucket - Additional comments

--- LeetCode Fb/MaximumSwap.py
+++ LeetCode Fb/MaximumSwap.py
@@ -11,27 +11,28 @@
 
 class Solution:
     def maximumSwap(self, num):
+        # convert the num into a list, so you can iterate through each number
         A = list(str(num))
         # display the index of each element but notice that this is a set
         # so, you will only see the indexes of the different numbers
         last = {int(x): i for i, x in enumerate(A)}
         # enumerate each element of the list 0:1, 1:7, 2:7, 3:7
         for i, x in enumerate(A):
-            # subtract with range(9, int(x),-1)
+            # start in 9, stop at int(x) and subtract 1, we use 9 because we have 9 values from 1-9
             for d in range(9, int(x), -1):
-                # if there's an element 'd' in the set which is replaced
-                # at position 0 and is greater than i then do the swap
-                if last.get(d, 0) > i:
+                # if there's an element 'd' in the set that is greater than the current index i then replace it
+                # by doing a swap. The get(d,0) indicates to get the numeric value. 
+                if last.get(d,0) > i:
                     # do the swap
                     A[i], A[last[d]] = A[last[d]], A[i]
                     return int("".join(A))
         return num
 
 def main():
-    number = 1777 # 7173 -> 7713
-    number2 = 2736
+    #num = 2273
+    num = 2003
     solution = Solution()
-    result = solution.maximumSwap(number2)
-    print(result)
+    res = solution.maximumSwap(num)
+    print(res)
 
 main()
