cba7cff37c144f2a54c959c214d74de0f7198b6a
Alejandro Bautista Ramos <alexbr9007@hotmail.com>
2020-06-12 17:41:21 +0000

    BSTIterator.py edited online with Bitbucket - Additional notes

--- LeetCode Fb/BSTIterator.py
+++ LeetCode Fb/BSTIterator.py
@@ -1,3 +1,30 @@
+"""
+    1. Push all the left nodes into a stack when you initialize the BSTIterator.
+    2. The stack will always keep the lowest value because we inserted all the left child nodes.
+    3. When you call next, you pop the lowest value and then you insert the right value. 
+    
+      6
+    4  10
+   2 5 8 12
+   
+   
+   2      
+   4   -> 4  -> 5  ->   ->    ->  8 ->     ->    -> done
+   6      6     6     6    10    10    10     12
+   
+   The number 2 doesn't have any child, so just pop it.
+   
+   The number 4 has a child (5), so pop 4 and then insert the child 5. 
+   
+   The number 5 doesn't have children, so pop the value.
+   The number 10 has 2 children, so insert 10 then you insert 8.
+   The number 8 doesn't have children, so pop the value.
+   The number 10 has a right child, so pop the value and insert 12. 
+   
+   
+   
+"""
+
 class BSTIterator:
     # @param root, a binary search tree's root node
     def __init__(self, root):
@@ -26,3 +53,9 @@ def main():
 # param_2 = obj.hasNext()
 
 main()
+"""
+
+Time complexity: O(N) where n is the number of nodes. 
+Space complexity: O(N)
+
+"""
