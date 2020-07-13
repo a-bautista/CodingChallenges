3ca3582c9ba182e88c37cd11c2eff2f7c888f267
Alejandro Bautista Ramos <alexbr9007@hotmail.com>
2020-07-13 16:07:13 +0000

    LongestSubstringWithoutRepeatingCharacters.py edited online with Bitbucket

--- LeetCode Fb/LongestSubstringWithoutRepeatingCharacters.py
+++ LeetCode Fb/LongestSubstringWithoutRepeatingCharacters.py
@@ -24,6 +24,20 @@ Example 3:
 
 
 '''
+#    def lengthOfLongestSubstring(self, s: str) -> int:
+#         used = {}
+#         max_length = start = 0
+#         for i, c in enumerate(s):
+#             if c in used and start <= used[c]:
+#                 # indicate where was the last poisition of the index
+#                 start = used[c] + 1
+#             else:
+#                 # add the length of the current index (total) - the last index that you had (existing content) + 1 (new val)
+#                 max_length = max(max_length, i - start + 1)
+#             # check where was the last index
+#             used[c] = i
+#         return max_length
+
 
 
 class Solution:
@@ -34,11 +48,22 @@ class Solution:
         """
         ans = 0
         sub = ''
+        
         for char in s:
+            # start adding each different char to the window
             if char not in sub:
                 sub += char
+                # calculate the length of the window and determine if the window is greater than ans
                 ans = max(ans, len(sub))
+            # if the char already exists in the window
             else:
+                # determine where is located this char in the window
                 cut = sub.index(char)
+                # cut the window from that char that you found and start a new window with this new chart
                 sub = sub[cut + 1:] + char
         return ans
+        
+'''
+    Time complexity: O(N)
+    Space complexity: O(k)
+'''
