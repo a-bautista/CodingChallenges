db802e7ef9d90169231d9c28dc1b61b17eded067
Alex BR <alex.bautista.ramos.90@gmail.com>
2020-08-10 15:59:39 -0600

    Added explanations to more exercises.

--- LeetCode Fb/CandyCrush1D.py
+++ LeetCode Fb/CandyCrush1D.py
@@ -1,33 +1,47 @@
 def candy_crush(input):
+    # edge case
     if not input:
         return input
     
     stack = []
+    # start adding the first letter
     stack.append([input[0], 1])
     
     for i in range(1, len(input)):
+        # if the next letter is different from the last letter in the stack
         if input[i] != input[i-1]:
+            # clear the stack only if the top letters in the stack have more than 3 letters
             if stack[-1][1] >= 3:
                 stack.pop()
+            # add the incoming letter in case it is the same as the one we have at the top of the stack
+            # this applies after a pop has been made
             if stack and stack[-1][0] == input[i]:
                 stack[-1][1] += 1
+            # add the incoming letter in case it is different as the one we have at the top of the stack
             else:
                 stack.append([input[i], 1])
+        # keep adding letters to the stack
         else:
             stack[-1][1] += 1
             
-    # handle end
+    # handle end in case you have more than 3 same letters at the end of the iteration
     if stack[-1][1] >= 3:
         stack.pop()
-        
+
+    # start printing the letters in the result
     out = []
     for ltrs in stack:
+    # append the result to the list
         out += ltrs[0] * ltrs[1]
     
     return ''.join(out)
     
-print(candy_crush("aaaabbbc")) #c
+#print(candy_crush("aaaabbbc")) #c
 print(candy_crush("aabbbacd")) #cd
-print(candy_crush("aabbccddeeedcba")) #blank expected
-print(candy_crush("aabbbaacd")) #cd
+#print(candy_crush("aabbccddeeedcba")) #blank expected
 print(candy_crush("dddabbbbaccccaax")) #x
+
+'''
+    Time complexity: O(nk)
+    Space complexity: O(1) because of the stack
+'''
--- LeetCode Fb/GenerateMines.py
+++ LeetCode Fb/GenerateMines.py
@@ -1,6 +1,6 @@
 '''
-    You are given a m*n grid. You are asked to generate k mines on this grid randomly. Each cell should
-    have equal probability of k / m*n of being chosen
+    You are given a m*n grid. You are asked to generate k mines on this grid randomly.
+    Each cell should have equal probability of k / m*n of being chosen
 
 '''
 
@@ -8,26 +8,37 @@ import random
 
 # m,n : Size of grid
 # k : Number of mines
-def plant(m,n,k):
-    s = m*n
-    cs = s
-    selected = []
-    grid = [0] * s
-    for i in range(k):
-        p = random.randint(0,cs-1)
-        pc = 0
-        for p2 in range(s):
-            if grid[p2]==0:
-                # Empty cell
-                if pc==p:
+def plant(m,n,mines):
+    board = m*n
+    delimiter_bomb = board
+    results = []
+    # [0,0,0,0,0,0,0] will hold the bombs as [1,0,0,0,1,0,1,0,0] in case m=n=k=3
+    vector = [0] * board
+
+    # start planting the mines in the vector
+    for i in range(mines):
+        bomb = random.randint(0,delimiter_bomb-1)
+        vector_position = 0
+        planted_bomb = 0
+        for i in range(board):
+            if vector[i]==0:
+                # Empty cell to plant a bomb
+                if vector_position==bomb:
                     # Found the p-th empty cell
+                    planted_bomb = i
                     break
-                pc += 1
-        selected.append(p2)
-        grid[p2] = 1 # Block the cell
-        cs -= 1
-    selected = [(s%m,s//m) for s in selected]
-    return selected
+                vector_position += 1
+
+        # contains the ith numbers where bombs are located in the vector
+        results.append(planted_bomb)
+        # plant the bomb in the vector
+        vector[planted_bomb] = 1 # Block the cell
+        # decrease the bomb delimiter because we have 1 space less once a bomb has been planted
+        delimiter_bomb -= 1
+
+    # i%m indicates the location of x and i//m indicates the location of y
+    results = [(i%m,i//m) for i in results]
+    return results
 
 
 def main():
