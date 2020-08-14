8170dfc751eaddba5babc16de491b276c766249a
a-bautista <alex.bautista.ramos.90@gmail.com>
2020-08-13 19:39:08 -0600

    added more explanations.

--- LeetCode Fb/MinimumEditDistance.py
+++ LeetCode Fb/MinimumEditDistance.py
@@ -32,10 +32,13 @@ class Solution:
         if n * m == 0:
             return n + m
 
-        # array to store the convertion history
+        # array to store the conversion history
+        # where each list represent the letters of word1 and the internal values represent the letters of word2
+        # d = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         d = [[0] * (m + 1) for _ in range(n + 1)]
 
         # init boundaries
+        # d = [[0, 1, 2, 3], [1, 0, 0, 0], [2, 0, 0, 0], [3, 0, 0, 0], [4, 0, 0, 0], [5, 0, 0, 0]]
         for i in range(n + 1):
             d[i][0] = i
         for j in range(m + 1):
@@ -45,13 +48,27 @@ class Solution:
         for i in range(1, n + 1):
             for j in range(1, m + 1):
                 left = d[i - 1][j] + 1
+                # down is computed only when we have the same letters
                 down = d[i][j - 1] + 1
                 left_down = d[i - 1][j - 1]
+                # if letter are different then we add 1 to left_down this value will be taken in the min
+                # this applies only when letters are different
                 if word1[i - 1] != word2[j - 1]:
                     left_down += 1
                 d[i][j] = min(left, down, left_down)
 
+        # the result will be given at the very end of the array
         return d[n][m]
+
+
+def main():
+    word1 = "horse"
+    word2 = "ros"
+    solution = Solution()
+    res = solution.minDistance(word1, word2)
+    print(res)
+
+main()
 
 '''
     Time complexity: O(mn)
