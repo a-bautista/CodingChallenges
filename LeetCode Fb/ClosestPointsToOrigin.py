d18f8d0e66399a1e0232fa574f8440a4a9c2387c
Alex BR <alex.bautista.ramos.90@gmail.com>
2020-08-13 20:48:08 -0600

    Added another solution to closest points to origin.

--- LeetCode Fb/ClosestPointsToOrigin.py
+++ LeetCode Fb/ClosestPointsToOrigin.py
@@ -9,14 +9,37 @@ from math import sqrt
         (1,p3), (2,p1)
         
         Keep the elements in a min heap
-    '''
+'''
+
+
+class Solution2:
+    def kClosest(self, points, K):
+        self.sort(points, 0, len(points) - 1, K)
+        return points[:K]
+
+    def sort(self, points, l, r, K):
+        if l < r:
+            p = self.partition(points, l, r)
+            if p == K:
+                return
+            elif p < K:
+                self.sort(points, p + 1, r, K)
+            else:
+                self.sort(points, l, p - 1, K)
+
+    def partition(self, points, l, r):
+        pivot = points[r]
+        a = l
+        for i in range(l, r):
+            if (points[i][0] ** 2 + points[i][1] ** 2) <= (pivot[0] ** 2 + pivot[1] ** 2):
+                points[a], points[i] = points[i], points[a]
+                a += 1
+        points[a], points[r] = points[r], points[a]
+        return a
 
 '''
-# elegant solution
-class Solution:
-    def kClosest(self, points, k):
-        return heapq.nsmallest(k, points,
-                               lambda p: p[0] * p[0] + p[1] * p[1])
+    Worst case the solution from above is O(N**2) with quicksort
+    But In average, we can reach in logN times and don't need to sort all elements at every step.
 '''
 
 class Solution:
